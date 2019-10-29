501060d248035124d6a3002fdc3e390889e2e3d4
julio-crypt0 <hernandezjulio58@ymail.com>
2019-10-29 12:06:40 -0700

    functional

--- server.js
+++ server.js
@@ -1,16 +1,49 @@
+require('dotenv').config();
 const express = require('express');
 const morgan = require('morgan');
+const cors = require('cors');
+const helmet = require('helmet');
 
 const app = express();
 
-app.use(morgan('dev'));
+const store = require('./movies-data-small');
+const API_TOKEN = process.env.API_TOKEN;
 
-app.use((req, res) => {
-  res.send('Hello, world!');
+app.use(morgan('common'));
+app.use(cors());
+app.use(helmet());
+
+function validateBearer(req, res, next) {
+  const authVal = req.get('Authorization') || '';
+  if (!authVal.startsWith('Bearer ')) {
+    return res.status(400).json({ error: 'Authorization token not found' });
+  }
+  const token = authVal.split(' ')[1];
+  if (token !== API_TOKEN) {
+    return res.status(401).json({ error: 'Token is invalid' });
+  }
+  next();
+}
+
+app.get('/movie', validateBearer, (req, res) => {
+  let movies = store;
+  const { genre, country, avg_vote } = req.query;
+  if (genre) {
+    movies = movies.filter((app) =>
+      app.genre.toLowerCase().includes(genre.toLowerCase()),
+    );
+  }
+  if (country) {
+    movies = movies.filter((app) =>
+      app.country.toLowerCase().includes(country.toLowerCase()),
+    );
+  }
+  if (avg_vote) {
+    movies = movies.filter((app) => app.avg_vote >= avg_vote);
+  }
+  res.json(movies);
 });
 
-const PORT = 8000;
-
-app.listen(PORT, () => {
-  console.log(`Server listening at http://localhost:${PORT}`);
+app.listen(8000, () => {
+  console.log(`Server listening at http://localhost:8000`);
 });
